8c241d61f7e66a35846b8935728b490985301e95
Mag <hicugi@gmail.com>
2021-11-03 16:25:12 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <AppBefore />
+  </div>
+</template>
+
+<script>
+import AppBefore from "./components/App/Before.vue";
+
+export default {
+  name: "App",
+  components: { AppBefore },
+};
+</script>
---
+++ src/components/App/Before.vue
@@ -0,0 +1,15 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+</style>
