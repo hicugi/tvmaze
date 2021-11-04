8e45c142b39cdaad1ed9dd77d46faa3f8343f06f
Mag <hicugi@gmail.com>
2021-11-04 17:25:13 +0600

    add search form

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="app">
     <AppBefore />
+
+    <Search />
   </div>
 </template>
 
 <script>
-import AppBefore from "./components/App/Before.vue";
+import AppBefore from "@/components/App/Before.vue";
+import Search from "@/components/Search/index.vue";
 
 export default {
   name: "App",
-  components: { AppBefore },
+  components: { AppBefore, Search },
+
+  data: () => ({ searchModel: "" }),
+
+  methods: {
+    handleClick() {
+      console.log(123);
+    },
+  },
 };
 </script>
---
+++ src/components/Search/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div :class="className">
+    <form :class="`${className}__container`" @submit.prevent="handleSubmit">
+      <UiInput v-model="keywords" type="search" placeholder="Search" />
+      <UiButton type="submit" variant="primary">Submit</UiButton>
+    </form>
+  </div>
+</template>
+
+<script>
+import UiButton from "@/components/Ui/Button.vue";
+import UiInput from "@/components/Ui/Input.vue";
+
+export default {
+  components: { UiInput, UiButton },
+
+  props: {
+    className: {
+      type: String,
+      default: "c-search",
+    },
+  },
+  data: () => ({
+    keywords: "",
+  }),
+
+  methods: {
+    handleSubmit() {},
+  },
+};
+</script>
+
+<style lang="scss">
+.c-search {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  background: $color-black;
+
+  &__container {
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: $container-gap;
+    padding-right: $container-gap;
+    max-width: $container-width;
+    display: flex;
+  }
+}
+</style>
