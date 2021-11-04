88582a59c72d41f37866eca4d82e911e1fc6a2fa
Mag <hicugi@gmail.com>
2021-11-04 17:08:02 +0600

    add input

---
+++ src/components/Ui/Input.vue
@@ -0,0 +1,39 @@
+<template>
+  <input
+    class="ui-input"
+    v-bind="{ type, value }"
+    v-on="$listeners"
+    @input="handleInput"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+  },
+  model: {
+    prop: "value",
+    event: "modelInput",
+  },
+
+  methods: {
+    handleInput({ target }) {
+      this.$emit("modelInput", target.value);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.ui-input {
+  padding: 7px 8px 8px;
+  border: 1px solid $color-gray;
+  border-radius: 0;
+  width: 100%;
+  height: auto;
+  display: block;
+  font-size: 16px;
+  line-height: 1.25em;
+}
+</style>
