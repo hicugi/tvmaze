5a83651cabc05a13f4d53a0c9b09ef22a015ca4e
Mag <hicugi@gmail.com>
2021-11-06 19:36:48 +0600

    feat: add horizontal scroll to shows list

--- src/components/Shows/Card.vue
+++ src/components/Shows/Card.vue
@@ -1,26 +1,23 @@
 <template>
   <div :class="computedClass">
-    <div :class="`${className}-image`" :style="imageStyles">
-      <img
-        v-if="visible"
-        :class="`${className}-image__img`"
-        :src="image"
-        :alt="name"
-      />
-    </div>
+    <template v-if="visible">
+      <div :class="`${className}-image`" :style="imageStyles">
+        <img :class="`${className}-image__img`" :src="image" :alt="name" />
+      </div>
 
-    <h3
-      v-if="!placeholder"
-      :class="`${className}__name`"
-      :title="name"
-      v-text="name"
-    />
-    <div
-      v-else
-      :class="`${className}__name-placeholder`"
-      :title="name"
-      v-text="name"
-    />
+      <h3
+        v-if="!placeholder"
+        :class="`${className}__name`"
+        :title="name"
+        v-text="name"
+      />
+      <div
+        v-else
+        :class="`${className}__name-placeholder`"
+        :title="name"
+        v-text="name"
+      />
+    </template>
   </div>
 </template>
 
@@ -57,79 +54,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.c-showsCard {
-  $name-height: 24px;
-
-  position: relative;
-  width: calc(#{$container-width} / 5 - 140px / 5);
-
-  &-image {
-    position: relative;
-    background: $color-gray no-repeat center;
-    background-size: cover;
-
-    &::before {
-      content: "";
-      padding-top: calc(100% * (12 / 9));
-      display: block;
-    }
-
-    &__img {
-      position: absolute;
-      left: 0;
-      top: 0;
-      width: 100%;
-      height: 100%;
-      opacity: 0.00001;
-    }
-  }
-
-  &__name,
-  &__name-placeholder {
-    margin: 8px 0 0;
-  }
-  &__name {
-    font-size: 18px;
-    line-height: $name-height;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-  }
-
-  &__name-placeholder {
-    position: relative;
-    height: $name-height;
-    background-color: $color-gray;
-    overflow: hidden;
-  }
-  &_placeholder &-image::after {
-    $size: 100px;
-    $animKey: cShowsCardImage;
-
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    border: 5px solid;
-    border-color: transparent transparent transparent $color-white;
-    border-radius: 100%;
-    width: $size;
-    height: $size;
-    animation: linear 1.5s $animKey infinite;
-    content: "";
-
-    @keyframes #{cShowsCardImage} {
-      from {
-        transform: translate(-50%, -50%) rotate(0deg);
-      }
-      to {
-        transform: translate(-50%, -50%) rotate(360deg);
-      }
-    }
-  }
-
-  @media (max-width: #{$container-width}) {
-    width: calc(100vw / 5 - 140px / 5);
-  }
-}
-</style>
+<style lang="scss" src="./Card.scss" />
--- src/components/Shows/List.vue
+++ src/components/Shows/List.vue
@@ -3,24 +3,40 @@
     <div :class="`${className}__container`">
       <h2 :class="`${className}__title`" v-text="title" />
 
-      <div :class="listClass">
-        <ul :class="`${className}-list__container`">
+      <div :class="`${className}-list`">
+        <ul :class="`${className}-list__container`" ref="sliderContainer">
           <!-- loading -->
           <template v-if="loading">
-            <template v-for="(item, index) in Array(maxVisibleItems).fill()">
-              <li :key="index" :class="`${className}-list__item`">
-                <ThisCard placeholder />
+            <template v-for="(item, index) in Array(visibleItemsLength).fill()">
+              <li :key="index" :class="listItemClass">
+                <ThisCard placeholder :visible="getCardVisibleProp(index)" />
               </li>
             </template>
           </template>
 
           <!-- items -->
           <template v-else v-for="(item, index) in items">
-            <li :key="index" :class="`${className}-list__item`">
+            <li :key="index" :class="listItemClass">
               <ThisCard v-bind="item" :visible="getCardVisibleProp(index)" />
             </li>
           </template>
         </ul>
+
+        <div v-if="!loading">
+          <button
+            v-show="scrollIndex > 0"
+            :class="[`${className}__arrow`, `${className}__arrow_prev`]"
+            type="button"
+            v-text="'next shows'"
+            @click="handlePrevClick"
+          />
+          <button
+            v-show="isNextBtnVisible"
+            :class="[`${className}__arrow`, `${className}__arrow_next`]"
+            v-text="'previous shows'"
+            @click="handleNextClick"
+          />
+        </div>
       </div>
     </div>
   </section>
@@ -28,6 +44,7 @@
 
 <script>
 import ThisCard from "./Card.vue";
+import cssExport from "./List.scss";
 
 export default {
   components: { ThisCard },
@@ -50,62 +67,70 @@ export default {
     },
   },
   data: () => ({
-    maxVisibleItems: 6,
+    visibleItemsLength: 5,
     scrollIndex: 0,
   }),
   computed: {
-    listClass() {
-      return ["", this.loading && "--loading"]
-        .filter((v) => typeof v === "string")
-        .map((v) => [this.className, "-list", v].join(""));
+    listItemClass() {
+      return `${this.className}-list__item`;
+    },
+
+    isNextBtnVisible() {
+      return (
+        this.items.length > (this.scrollIndex + 1) * this.visibleItemsLength
+      );
+    },
+    minMaxScrollIndexes() {
+      const maxItems = this.visibleItemsLength;
+      const { scrollIndex } = this;
+
+      const min =
+        (scrollIndex ? scrollIndex - 1 : scrollIndex * (maxItems + 1)) - 2;
+      const max = (scrollIndex + 2) * maxItems;
+
+      return [min, max];
     },
   },
 
   methods: {
     getCardVisibleProp(index) {
-      return (this.scrollIndex + 1) * this.maxVisibleItems > index;
+      const [min, max] = this.minMaxScrollIndexes;
+      return min <= index && index < max;
+    },
+
+    getNextSlideItem(index) {
+      const { sliderContainer } = this.$refs;
+      if (!sliderContainer) return;
+
+      const { visibleItemsLength } = this;
+
+      const nthIndex = index * visibleItemsLength + 1;
+      const selector = `li:nth-child(${nthIndex})`;
+      return sliderContainer.querySelector(selector);
+    },
+
+    scrollSlider(newIndex) {
+      const { sliderContainer } = this.$refs;
+
+      const item = sliderContainer.querySelector(`.${this.listItemClass}`);
+      if (!item) return;
+
+      const containerGap = parseInt(cssExport.containerGap.replace("px", ""));
+      const scrollToItem = this.getNextSlideItem(newIndex);
+      const scrollToX = scrollToItem.offsetLeft - containerGap;
+
+      sliderContainer.scrollTo(scrollToX, 0);
+
+      this.scrollIndex = newIndex;
+    },
+    handlePrevClick() {
+      const nextIndex = this.scrollIndex - 1;
+      this.scrollSlider(nextIndex);
+    },
+    handleNextClick() {
+      const prevIndex = this.scrollIndex + 1;
+      this.scrollSlider(prevIndex);
     },
   },
 };
 </script>
-
-<style lang="scss">
-.c-showsList {
-  margin-bottom: 10px;
-  overflow: hidden;
-
-  &__container {
-    margin-left: auto;
-    margin-right: auto;
-    padding-left: $container-gap;
-    padding-right: $container-gap;
-    max-width: $container-width;
-  }
-
-  &__title {
-    margin: 40px 0 15px;
-    font-size: 26px;
-    left: 1.25em;
-  }
-
-  &-list {
-    $gap: 20px;
-
-    max-width: #{$container-width - $container-gap * 2};
-    display: block;
-
-    &__container {
-      margin: 0;
-      padding: 0;
-      list-style: none;
-      display: flex;
-    }
-
-    &__item {
-      & + & {
-        margin-left: $gap;
-      }
-    }
-  }
-}
-</style>
